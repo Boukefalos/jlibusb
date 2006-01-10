8c89294a49429c0728ea7d896b2c94e736913292
schlaepfer <schlaepfer@94ad28fe-ef68-46b1-9651-e7ae4fcf1c4c>
2006-01-10 15:59:47 +0000

    - setting log Level fixed (also set Level of Handler)
    git-svn-id: https://svn.code.sf.net/p/libusbjava/code/trunk@74 94ad28fe-ef68-46b1-9651-e7ae4fcf1c4c

--- mcdp/src/ch/ntb/mcdp/utils/logger/LogUtil.java
+++ mcdp/src/ch/ntb/mcdp/utils/logger/LogUtil.java
@@ -1,15 +1,33 @@
 package ch.ntb.mcdp.utils.logger;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+
+
 public class LogUtil {
 
-	public static McdpLogger ch_ntb_mcdp_bdi, ch_ntb_mcdp_bdi_test;
-
+	public static McdpLogger ch_ntb_mcdp_bdi, ch_ntb_mcdp_bdi_test, ch_ntb_mcdp_usb;
+	
+	private static McdpLogger getLogger(String name, Level loglevel) {
+		McdpLogger logger = McdpLogger.getLogger(name);
+		Handler[] h = logger.getHandlers();
+		for (int i = 0; i < h.length; i++) {
+			logger.removeHandler(h[i]);
+		}
+		Handler console = new ConsoleHandler();
+		console.setLevel(loglevel);
+		logger.addHandler(console);
+		logger.setLevel(loglevel);
+		return logger;
+	}
+	
 	static {
 		// set all loglevels here
-		ch_ntb_mcdp_bdi = McdpLogger.getLogger("ch.ntb.mcdp.bdi");
-		ch_ntb_mcdp_bdi.setLevel(LogLevel.ALL);
+		ch_ntb_mcdp_bdi = getLogger("ch.ntb.mcdp.bdi", LogLevel.ALL);
+		
+		ch_ntb_mcdp_bdi_test = getLogger("ch.ntb.mcdp.bdi.test", LogLevel.OFF);
 
-		ch_ntb_mcdp_bdi_test = McdpLogger.getLogger("ch.ntb.mcdp.bdi.test");
-		ch_ntb_mcdp_bdi_test.setLevel(LogLevel.ALL);
+		ch_ntb_mcdp_usb = getLogger("ch.ntb.mcdp.usb", LogLevel.OFF);
 	}
 }
