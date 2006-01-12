77c16954d84336916ae548bb4c49b82b8699b65e
schlaepfer <schlaepfer@94ad28fe-ef68-46b1-9651-e7ae4fcf1c4c>
2006-01-12 08:11:52 +0000

    - setLevel added - Level-handling fixed
    git-svn-id: https://svn.code.sf.net/p/libusbjava/code/trunk@83 94ad28fe-ef68-46b1-9651-e7ae4fcf1c4c

--- mcdp/src/ch/ntb/mcdp/utils/logger/LogUtil.java
+++ mcdp/src/ch/ntb/mcdp/utils/logger/LogUtil.java
@@ -4,13 +4,29 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 
-
 public class LogUtil {
 
-	public static McdpLogger ch_ntb_mcdp_bdi, ch_ntb_mcdp_bdi_test, ch_ntb_mcdp_usb;
-	
-	private static McdpLogger getLogger(String name, Level loglevel) {
-		McdpLogger logger = McdpLogger.getLogger(name);
+	public static McdpLogger ch_ntb_mcdp_bdi, ch_ntb_mcdp_bdi_test,
+			ch_ntb_mcdp_usb;
+
+	static {
+		// set all loglevels here
+		ch_ntb_mcdp_usb = getLogger("ch.ntb.mcdp.usb", LogLevel.OFF);
+
+		ch_ntb_mcdp_bdi = getLogger("ch.ntb.mcdp.bdi", LogLevel.ALL);
+
+		ch_ntb_mcdp_bdi_test = getLogger("ch.ntb.mcdp.bdi.test", LogLevel.ALL);
+	}
+
+	public static void setLevel(McdpLogger logger, Level loglevel) {
+		Handler[] h = logger.getHandlers();
+		for (int i = 0; i < h.length; i++) {
+			h[i].setLevel(loglevel);
+		}
+		logger.setLevel(loglevel);
+	}
+
+	private static void initLevel(McdpLogger logger, Level loglevel) {
 		Handler[] h = logger.getHandlers();
 		for (int i = 0; i < h.length; i++) {
 			logger.removeHandler(h[i]);
@@ -19,15 +35,12 @@ public class LogUtil {
 		console.setLevel(loglevel);
 		logger.addHandler(console);
 		logger.setLevel(loglevel);
-		return logger;
+		logger.setUseParentHandlers(false);
 	}
-	
-	static {
-		// set all loglevels here
-		ch_ntb_mcdp_bdi = getLogger("ch.ntb.mcdp.bdi", LogLevel.ALL);
-		
-		ch_ntb_mcdp_bdi_test = getLogger("ch.ntb.mcdp.bdi.test", LogLevel.OFF);
 
-		ch_ntb_mcdp_usb = getLogger("ch.ntb.mcdp.usb", LogLevel.OFF);
+	private static McdpLogger getLogger(String name, Level loglevel) {
+		McdpLogger logger = McdpLogger.getLogger(name);
+		initLevel(logger, loglevel);
+		return logger;
 	}
 }
